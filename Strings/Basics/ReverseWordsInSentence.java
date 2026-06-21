d20f0392cad8485f4a2dd840fbdadd917ff2be11
Deepanshu-Sati <deepanshu12official@gmail.com>
2026-06-21 23:19:57 +0530

    Added solution to problem, Reverse words in a given string

---
+++ Strings/Basics/ReverseWordsInSentence.java
@@ -0,0 +1,49 @@
+package Strings.Basics;
+
+/*
+ * Link             : https://leetcode.com/problems/reverse-words-in-a-string/
+ * Problem          : Reverse words in a given string / palindrome check
+ * Difficulty       : Easy-Medium
+ * Approach         : Two pointer
+ * Time complexity  : O(N)
+ * Space complexity : O(N)
+ */
+
+class ReverseWordsInSentence {
+    static String reverseWords(String s) {
+
+        StringBuilder result = new StringBuilder();
+        int i = s.length()-1;
+        
+        while(i>=0){
+
+            //skipping through spaces
+            
+            while (i>=0 && s.charAt(i) == ' '){
+                i--;
+            }
+
+            if (i<0){
+                break;
+            }
+
+            int j = i;
+            // find beginning of the word
+            while(j>=0 && s.charAt(j)!=' '){
+                j--;
+            }
+
+            result.append(s.substring(j+1,i+1));
+            result.append(" ");
+
+            i=j-1;
+        }
+        result.deleteCharAt(result.length()-1);
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "  Hello   World    ";
+        System.out.println(reverseWords(s));
+    }
+}
